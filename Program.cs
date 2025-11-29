06e42202dabefb25c0bdaafbe802b7cc685bb11a
EngAlaaMahmoud <EngAlaaMahmoud@github.com>
2025-11-29 22:44:21 +0200

    Revert "etax"
    This reverts commit c603a75192ef5f9ef6be3f7e1ee815436703bf26.

--- Program.cs
+++
@@ -1,5 +0,0 @@
-builder.Services.AddHttpClient<IETATaxService, ETATaxClientService>(client =>
-{
-    // Base address optional, ETATaxClientService builds full URLs from config
-    client.Timeout = TimeSpan.FromSeconds(30);
-});
