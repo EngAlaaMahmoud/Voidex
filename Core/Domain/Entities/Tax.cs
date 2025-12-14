3c51e498fa880643057d9edb54ab93d884951b06
yacoup <yacoubmgy@gmail.com>
2025-12-14 20:13:28 +0200

    taxscreen

--- Core/Domain/Entities/Tax.cs
+++ Core/Domain/Entities/Tax.cs
@@ -7,10 +7,14 @@ public class Tax : BaseEntity
     public string? Name { get; set; }
     public double? Percentage { get; set; }
     public string? Description { get; set; }
+
+    // New fields for tax register
+    public string? MainCode { get; set; }
+    public string? TypeCode { get; set; }
+    public string? SubCode { get; set; }
+    public string? TypeName { get; set; }
+
     public virtual ICollection<SalesOrderTax> SalesOrderTaxes { get; set; } = new List<SalesOrderTax>();
     public virtual ICollection<PurchaseOrderTax> PurchaseOrderTaxes { get; set; } = new List<PurchaseOrderTax>();
     public ICollection<Product>? Products { get; set; } = new List<Product>();
-
-
-
 }
